1aa24f7d924275b715120439cbd4261c9611b87c
Rishicalicut <rishikeshcalicut123@gmail.com>
2023-03-07 18:39:24 +0530

    first commit -- react ecommerce application

---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    
+`
+
+const Products = () => {
+  return (
+    <Container>
+    
+    </Container>
+  )
+}
+
+export default Products
---
+++ src/data.js
@@ -0,0 +1,103 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img : "https://images.unsplash.com/photo-1567855354833-ac2c4f967b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1781&q=80",
+
+        title : "Summer Sale",
+        desc : "Welcome to WishBin , online store of College of Engineering Chengannur",
+        bg: "fcf1ed"
+    },
+    {
+        id :2,
+        img: "https://images.unsplash.com/photo-1568205612837-017257d2310a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+
+        title: "Buy now with the click of a button",
+        desc: "Convenience at your fingertips.",
+        bg: "f5fafd",
+    },
+
+    {
+        id:3,
+        img :"https://images.unsplash.com/photo-1601001435957-74f0958a93fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+
+        title: "Hassle free Shopping now made possible",
+        desc: " Pick up at your convenience",
+        bg: "fcf1ed"
+    },
+
+];
+
+
+export const categories=[
+
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1593435221502-c5d7bfc26cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+        title : "STATIONERY",
+    },
+
+    {
+        id:2,
+        img:"https://images.unsplash.com/photo-1585244759837-5bb4b3a6919c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+        title : "BOOKS AND RECORDS",
+    },
+
+    {
+        id:3,
+        img: "https://images.unsplash.com/photo-1624627294477-1c0860983b2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+        title : "OTHER PRODUCTS",
+    },
+
+];
+
+
+export const popularProducts =[
+
+    {
+        // COLLEGE NOTEBOOK 
+        id:1,
+        img:"https://m.media-amazon.com/images/I/71qMFxtcfdL._SL1500_.jpg"
+    },
+
+    {
+        //SMALL NOTEBOOK
+        id:2,
+        img:"https://cdn.shopify.com/s/files/1/0486/4137/6411/products/NewProject_22_df009e6f-90c8-44f6-81bf-89d9ea65df70.jpg?v=1634640423"
+    },
+
+    {
+        //PEN-BLUE
+        id:3,
+        img:"https://www.bigbasket.com/media/uploads/p/l/40121160_2-bic-cello-technotip-ball-pen-blue.jpg"
+    },
+
+    {   //PEN-BLACK
+        id:4,
+        img:"https://m.media-amazon.com/images/I/81esZ5d2ifL._SL1500_.jpg"
+    },
+
+    {   //PENCIL
+        id:5,
+        img:"https://m.media-amazon.com/images/I/61V+fRrcReL._SX522_.jpg"
+    },
+
+    {   //ERASER
+        id:6,
+        img:"https://m.media-amazon.com/images/I/51vAN6zX72L._SX522_.jpg"
+    },
+
+    {
+        //RULER
+        id:7,
+        img:"https://m.media-amazon.com/images/I/61FTphNUfCS._SX522_.jpg"
+    },
+
+    {
+        //CHOCOLATE -- kit-kat
+        id:8,
+        img:"https://m.media-amazon.com/images/I/71MZdEMSOaL._SX679_.jpg"
+    },
+
+]
+
+
